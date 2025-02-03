46c3d39b577aee531472f2144b7a326a7be9e439
Seu Nome <seu-email@example.com>
2025-02-03 13:44:31 -0300

    atualização

--- admin/src/service/api.js
+++ admin/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4400/api',
+  baseURL: 'central-jovem-production.up.railway.app',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
