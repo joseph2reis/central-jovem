93646760e8ccb0de4312d0aa94eadc7c22db1730
Seu Nome <seu-email@example.com>
2025-02-09 16:06:22 -0300

    correção de bugs

--- admin/src/service/api.js
+++ admin/src/service/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL:  'https://central-jovem-production.up.railway.app/api',
-  timeout: 1000,
+  timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
   }
